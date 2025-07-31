cfe102b12b8d573cf35b2f71284b9f459f6c8dc8
Tracey <tracey@example.com>
2025-07-31 02:05:15 -0400

    Clean commit: removed leaked token

--- flake.nix
+++ flake.nix
@@ -1,48 +1,57 @@
-outputs = { self, nixpkgs, flake-utils }:
-  flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs { inherit system; };
-    in {
-      packages.default = pkgs.stdenv.mkDerivation {
-        pname = "leafpad";
-        version = "0.8.18.1-tracey";
+{
+  description = "Tracey's Leafpad remix flake";
 
-        src = ./leafpad-nix/leafpad-src;
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
-        nativeBuildInputs = [
-          pkgs.pkg-config
-          pkgs.automake
-          pkgs.autoconf
-          pkgs.libtool
-          pkgs.intltool
-        ];
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        packages.default = pkgs.stdenv.mkDerivation {
+          pname = "leafpad";
+          version = "0.8.18.1-tracey";
 
-        buildInputs = [ pkgs.gtk2 ];
+          src = ./leafpad-nix/leafpad-src;
 
-        configurePhase = ''
-          ./autogen.sh || autoreconf -vi
-          ./configure --prefix=$out
-        '';
+          nativeBuildInputs = [
+            pkgs.pkg-config
+            pkgs.automake
+            pkgs.autoconf
+            pkgs.libtool
+            pkgs.intltool
+          ];
 
-        buildPhase = "make";
-        installPhase = "make install";
+          buildInputs = [ pkgs.gtk2 ];
 
-        meta = {
-          description = "Custom Leafpad build by Tracey";
-          license = pkgs.lib.licenses.gpl2;
-          maintainers = [];
+          configurePhase = ''
+            ./autogen.sh || autoreconf -vi
+            ./configure --prefix=$out
+          '';
+
+          buildPhase = "make";
+          installPhase = "make install";
+
+          meta = {
+            description = "Custom Leafpad build by Tracey";
+            license = pkgs.lib.licenses.gpl2;
+            maintainers = [];
+          };
         };
-      };
 
-      devShells.default = pkgs.mkShell {
-        buildInputs = [
-          pkgs.pkg-config
-          pkgs.automake
-          pkgs.autoconf
-          pkgs.libtool
-          pkgs.intltool
-          pkgs.gtk2
-        ];
-      };
-    }
-  );
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.pkg-config
+            pkgs.automake
+            pkgs.autoconf
+            pkgs.libtool
+            pkgs.intltool
+            pkgs.gtk2
+          ];
+        };
+      }
+    );
+}
